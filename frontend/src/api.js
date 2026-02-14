9466d4e0f300fa7a7cc111138fc38992a7dfd626
OlegFPY85 <https://github.com/OlegFPY85>
2026-02-14 15:19:46 +0300

    update_global

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://89.104.67.224/api/';
+const API_URL = import.meta.env.VITE_API_URL
 
 export const registerUser = async (userData) => {
     return await axios.post(`${API_URL}auth/register/`, userData);
